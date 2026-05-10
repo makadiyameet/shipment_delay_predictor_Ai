182e65519a6712cea7761b2832ad4e9cd445d1aa
makadiyameet <makadiyameet1@gmail.com>
2026-05-10 16:50:34 +0200

    Update Groq model

--- api/chat.js
+++ api/chat.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
         "Authorization": `Bearer ${process.env.GROQ_API_KEY}`
       },
       body: JSON.stringify({
-        model: "mixtral-8x7b-32768",
+        model: "llama-3.3-70b-versatile",
         temperature: 0.2,
         messages: [
           {
